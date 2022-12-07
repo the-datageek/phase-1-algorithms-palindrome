50668d7ccece09b51ff6f7adef48fbefe6aa78c3
Faith Moraa <moraaf770@gmail.com>
2022-12-07 15:16:14 +0300

    completed assignemnt

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-function isPalindrome(word) {
+
+function isPalindrome(string) {
   // Write your algorithm here
+  const len = string.length; 
+  for(let i=0; i<len/2; i++){
+    if(string[i] !== string[len-1-i]){
+      return false
+    }
+  }
+  return true
 }
+console.log(isPalindrome('abba'))
+console.log(isPalindrome('words'))
 
-/* 
-  Add your pseudocode here
-*/
 
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
